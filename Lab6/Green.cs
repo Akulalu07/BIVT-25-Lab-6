0cf2af78d8e3976ad08c1dcdc94d4910b6ce7e92
Luka <lukapuhlyak8@gmail.com>
2025-12-14 17:13:00 +0300

    half ~> work

--- Lab6/Green.cs
+++ Lab6/Green.cs
@@ -9,7 +9,9 @@ namespace Lab6
         {
 
             // code here
-
+            DeleteMaxElement(ref A);
+            DeleteMaxElement(ref B);
+            A = CombineArrays(A,B);
             // end
 
         }
@@ -17,6 +19,20 @@ namespace Lab6
         {
 
             // code here
+            int rows = matrix.GetLength(0);
+
+            int limit = Math.Min(rows, array.Length);
+
+            for (int i = 0; i < limit; i++)
+            {
+                int maxCol;
+                int max = FindMaxInRow(matrix, i, out maxCol);
+
+                if (max < array[i])
+                {
+                    matrix[i, maxCol] = array[i];
+                }
+            }
 
             // end
 
@@ -25,7 +41,12 @@ namespace Lab6
         {
 
             // code here
-
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+                return; 
+            
+            int row, col;
+            FindMax(matrix, out row, out col);
+            SwapColWithDiagonal(matrix, col);
             // end
 
         }
@@ -33,7 +54,17 @@ namespace Lab6
         {
 
             // code here
+            for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+            {
+                bool hasZero = false;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (matrix[i, j] == 0)
+                        hasZero = true;
+                        break;
 
+                if (hasZero)
+                    RemoveRow(ref matrix, i);
+            }
             // end
 
         }
@@ -42,7 +73,10 @@ namespace Lab6
             int[] answer = null;
 
             // code here
-
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+                return; answer;
+            
+            answer = GetRowsMinElements(matrix);
             // end
 
             return answer;
@@ -52,7 +86,7 @@ namespace Lab6
             int[] answer = null;
 
             // code here
-
+            answer = CombineArrays( SumPositiveElementsInColumns(A),SumPositiveElementsInColumns(B));
             // end
 
             return answer;
@@ -92,6 +126,134 @@ namespace Lab6
             // end
 
             return res;
+        }
+
+
+        
+        public void DeleteMaxElement(ref int[] array)
+        {
+            if (array == null || array.Length == 0)
+                return;
+
+            int max = array.Max();
+            int maxindex = Array.IndexOf(array, max);
+            array = array
+                .Where((x, i) => i != maxindex)
+                .ToArray();
+        }
+
+        public int[] CombineArrays(int[] A, int[] B)
+        {
+            int[] C = new int[A.Length + B.Length];
+            int j = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                C[j] = A[i];
+                j++;
+            }
+
+            for (int i = 0; i < B.Length; i++)
+            {
+                C[j] = B[i];
+                j++;
+            }
+
+            return C;
+        }
+        public int FindMaxInRow(int[,] matrix, int row, out int col)
+        {
+            int cols = matrix.GetLength(1);
+
+            int max = matrix[row, 0];
+            col = 0;
+
+            for (int j = 1; j < cols; j++)
+            {
+                if (matrix[row, j] > max)
+                {
+                    max = matrix[row, j];
+                    col = j;
+                }
+            }
+
+            return max;
+        }
+
+        public void FindMax(int[,] matrix, out int row, out int col)
+        {
+            row = 0;
+            col = 0;
+            int max = matrix[0,0];
+
+            for (int i=0; i<matrix.GetLength(0);i++)
+                for (int j=0; j<matrix.GetLength(1);j++)
+                    if (max<matrix[i,j]){
+                        row = i;
+                        col = j;
+                        max = matrix[i,j]
+                    }
+                
+
+        }
+
+        public void SwapColWithDiagonal(int[,] matrix, int col)
+        {
+            for (int i=0; i<matrix.GetLength(0);i++)
+            {
+                (matrix[i,col], matrix[i,i] )= (matrix[i,i], matrix[i,col]);
+            }
+        }
+        public void RemoveRow(ref int[,] matrix, int row)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] newMatrix = new int[rows - 1, cols];
+            int k = 0;
+            for (int i=0; i< rows; i++)
+            {
+                if (i==row) {continue;}
+                for (int j=0; j<cols; j++)
+                {
+                    newMatrix[k,j] = matrix[i,j];
+                }
+                k++;
+            }
+            matrix = newMatrix;
+        }
+        public int[] GetRowsMinElements(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[] resp = new int[rows];
+            for (int i=0; i<rows; i++) {
+                int min = matrix[i,i];
+                for (int j=i; j<cols; j++){
+                    if (min>matrix[i,j]){
+                        min = matrix[i,j];
+                    }
+                }
+                resp[i] = min;
+            }
+            return resp;
+        }
+
+        public int[] SumPositiveElementsInColumns(int[,] matrix) 
+        {
+            int rows = matrix.GetLength(0); 
+            int cols = matrix.GetLength(1);
+            int[] ans = new int[cols];
+            for (int i = 0; i< rows; i++)
+            {
+              for (int j = 0; j<cols; j++)
+              {
+                if (matrix[i,j] > 0) 
+                {
+                  ans[j] += matrix[i,j];
+                }
+              }
+            }
+            return ans;
         }
     }
 }
